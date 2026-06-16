dba2202c003ad28f5c063dea05ddb78429926c70
Kushang Garg <kushanggarg2001@gmail.com>
2026-06-16 19:09:34 +0530

    added exception handling

---
+++ src/main/java/com/kushang/jobportal/exception/ResourceNotFoundException.java
@@ -0,0 +1,8 @@
+package com.kushang.jobportal.exception;
+
+public class ResourceNotFoundException extends RuntimeException {
+
+    public ResourceNotFoundException(String message) {
+        super(message);
+    }
+}
