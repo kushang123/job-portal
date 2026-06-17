59f2b660a9c36bb316c1e15e0a2ce89a3f892d06
Kushang Garg <kushanggarg2001@gmail.com>
2026-06-17 17:09:57 +0530

    Added custom exceptions for register/login flow

---
+++ src/main/java/com/kushang/jobportal/exception/AdminRegistrationException.java
@@ -0,0 +1,8 @@
+package com.kushang.jobportal.exception;
+
+public class AdminRegistrationException  extends RuntimeException {
+
+    public AdminRegistrationException(String message) {
+        super(message);
+    }
+}
