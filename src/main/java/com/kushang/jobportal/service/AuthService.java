3d96165580bc993e89206ba088a274fdfb3a913d
Kushang Garg <kushanggarg2001@gmail.com>
2026-06-17 14:54:45 +0530

    Completed login and registration

---
+++ src/main/java/com/kushang/jobportal/service/AuthService.java
@@ -0,0 +1,56 @@
+package com.kushang.jobportal.service;
+
+import com.kushang.jobportal.entity.User;
+import com.kushang.jobportal.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+import com.kushang.jobportal.dto.RegisterRequest;
+import com.kushang.jobportal.dto.LoginRequest;
+import com.kushang.jobportal.entity.Role;
+import com.kushang.jobportal.security.JwtUtil;
+import com.kushang.jobportal.exception.InvalidCredentialsException;
+
+
+@Service
+public class AuthService {
+
+    @Autowired
+    private UserRepository userRepository;
+
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+
+    @Autowired
+    private JwtUtil jwtUtil;
+
+    public String registerUser(RegisterRequest request) {
+        if (userRepository.existsByEmail(request.getEmail())) {
+            return "Email already registered!";
+        }
+
+        if (request.getRole() == Role.ADMIN) {
+            return "Self-registration as ADMIN is not allowed!";
+        }
+
+        User user = new User();
+        user.setName(request.getName());
+        user.setEmail(request.getEmail());
+        user.setPassword(passwordEncoder.encode(request.getPassword()));
+        user.setRole(request.getRole());
+
+        userRepository.save(user);
+        return "User registered successfully!";
+    }
+    public String login(LoginRequest request) {
+        User user = userRepository.findByEmail(request.getEmail())
+                .orElseThrow(() -> new InvalidCredentialsException("Invalid email or password"));
+
+        if (!passwordEncoder.matches(request.getPassword(), user.getPassword())) {
+            throw new InvalidCredentialsException("Invalid email or password");
+        }
+
+        return jwtUtil.generateToken(user.getEmail(), user.getRole().name());
+    }
+
+}
