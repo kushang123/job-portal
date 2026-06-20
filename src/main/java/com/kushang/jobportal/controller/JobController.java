ac0d77ccbfa36e6eb958045f8a97d0a88e6febe8
Kushang Garg <kushanggarg2001@gmail.com>
2026-06-20 12:03:02 +0530

    completed job layer.

--- src/main/java/com/kushang/jobportal/controller/JobController.java
+++ src/main/java/com/kushang/jobportal/controller/JobController.java
@@ -1,5 +1,5 @@
 package com.kushang.jobportal.controller;
-
+import java.util.*;
 import com.kushang.jobportal.dto.JobRequest;
 import com.kushang.jobportal.dto.JobResponse;
 import com.kushang.jobportal.service.JobService;
@@ -25,4 +25,37 @@ public class JobController {
         JobResponse response = jobService.createJob(request);
         return new ResponseEntity<>(response, HttpStatus.CREATED);
     }
+
+    @GetMapping
+    public ResponseEntity<List<JobResponse>> getAllOpenJobs() {
+        List<JobResponse> jobs = jobService.getAllOpenJobs();
+        return new ResponseEntity<>(jobs, HttpStatus.OK);
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<JobResponse> getJobById(@PathVariable Long id) {
+        JobResponse response = jobService.getJobById(id);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @GetMapping("/company")
+    public ResponseEntity<List<JobResponse>> getJobsByCompanyName(@RequestParam String name) {
+        List<JobResponse> jobs = jobService.getJobsByCompanyName(name);
+        return new ResponseEntity<>(jobs, HttpStatus.OK);
+    }
+
+    @PutMapping("/{id}")
+    @PreAuthorize("hasRole('COMPANY')")
+    public ResponseEntity<JobResponse> updateJob(@PathVariable Long id, @RequestBody JobRequest request) {
+        JobResponse response = jobService.updateJob(id, request);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
+    @PatchMapping("/{id}/close")
+    @PreAuthorize("hasRole('COMPANY')")
+    public ResponseEntity<JobResponse> closeJob(@PathVariable Long id) {
+        JobResponse response = jobService.closeJob(id);
+        return new ResponseEntity<>(response, HttpStatus.OK);
+    }
+
 }
